aaf13b32c1ec6bc30691a1d7b65b1c5ef09f5ebc
juanmanuel060207@gmail.com <juanmanuel060207@gmail.com>
2024-09-19 11:26:41 -0300

    PUNTO 2 Y 3

---
+++ src/PUNTO_2_Y_3.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#ifndef _PUNTO1_H_
+#define _PUNTO1_H_
+
+
+void mostrado(uint8_t i, uint8_t val_fila, uint8_t cant);
+
+void enviar_mensaje(uint8_t *columna, const uint8_t *mensaje);
+
+#endif
