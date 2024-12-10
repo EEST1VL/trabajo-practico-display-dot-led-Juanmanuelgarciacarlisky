68bfce143bedbe58e3bccc536d8636175841f71b
Juanmanuelgarciacarlisky <juanmanuel060207@gmail.com>
2024-12-10 11:38:33 -0300

    Update DISPLAY_DOT.h

--- include/DISPLAY_DOT.h
+++ include/DISPLAY_DOT.h
@@ -1,35 +1,65 @@
 #include <Arduino.h>
 
-extern uint8_t fila[7];
-extern char msj[];
-extern uint8_t columna[128];
-extern uint8_t FILA;
-extern uint8_t p;
-extern uint8_t aux;
-extern uint8_t cnt;
-extern uint16_t flag_tiempo, flag_END_TIME, flag_titilado;
-extern uint8_t total_ms, flag1, flag2, flag3, old_cnt;
-extern uint8_t flag_tiempo_suma, flag_tiempo_resta;
-extern bool flag_estado_END_TIME;
-extern uint16_t tiempo;
-extern uint16_t cnt2;
+typedef enum
+{
+  STOP,
+  START,
+  ANALOGICO_DIGITAL,
+  END_TIME,
+  BOTON_SPEED
+} ESTADO;
+ESTADO estado;
+uint16_t cnt = 2;
 
-void Registro_de_desplazamiento(uint64_t val, uint8_t cant);
+int16_t angle;
+uint16_t old_angle;
 
-void config_TIMER0(void);
+uint8_t p;
+uint8_t fila[7]{0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+uint8_t FILA;
+uint8_t i;
+uint8_t columna2[128]{48, 46, 48, 48, 86},
+msj2[] = {48, 46, 48, 48, 86};
+uint8_t columna[128]{
+    48,
+    48,
+    109,
+    58,
+    48,
+};
+char msj[] = {48, 48, 109, 58, 48, 48, 115};
+//uint8_t columna[128]{
 
+//},
+//msj[100] = {};
+uint8_t cnt2;
+uint8_t CONTADOR;
+uint8_t cant_fila;
+uint8_t val_fila;
+uint8_t mensaje, temporal;
+uint32_t valorAdc;
+uint16_t flag_tiempo, flag_END_TIME, flag_titilado, global, temperatura2, flag;
+uint8_t total_ms = 50, flag1, flag2, flag3, old_cnt, flag4, flag5;
+uint8_t flag_tiempo_suma, flag_tiempo_resta;
+bool flag_estado_END_TIME;
+uint16_t tiempo;
+
+/*Funciones */
+void Config_Puertos(void);
+
+void Conversion(uint16_t temperatura2);
 void enviar_mensaje(uint8_t *columna, const char *mensaje);
-
-void mostrado(uint8_t i, uint8_t val_fila, uint8_t cant);
-
+void mostrado(uint8_t i, uint8_t val_fila, uint8_t cant,uint8_t *columna);
 void Multiplexado(uint16_t num);
+void config_TIMER0(void);
+/*Funciones relacionadas a interrupciones */
+void initInterrupts(void);
 
- void enable(void);
-
- void puertos(void);
-
- void barrido_display(void);
-
- void Multiplexado_filas(void);
-
- void pulsadores(void);
+/*
+ * TABLAS PRIVADAS AL MODULO
+ */
+/**
+ * @brief Tabla de caracteres para mostrar en la matriz de puntos
+ * @date ${date}
+ * @author Nicolas Ferragamo nferragamo@frba.utn.edu.ar
+ */
